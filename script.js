6408d888e147fcded6d5fec7c5c01e620c7acd94
desert-bloom <sydney.c.spiegel@gmail.com>
2024-10-22 17:28:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set today's password and message
-let correctPassword = "10212024";  // You can change this each day
-let messageToShow = "My Dearest Griselda,\n\nThank you so much for sending me that beautiful video about love being the truest kind of friendship. It really touched my heart and made me think about how lucky I am to have you in my life. Our connection truly is built on the foundation of trust, kindness, and understanding, and that means everything to me.\n\nYou make my world brighter every day, and I’m so grateful for the love and friendship we share.\n\nWith all my love,\nSydney";
+let correctPassword = "102324";  // You can change this each day
+let messageToShow = "....pst. Hey I love you\n\nWith all my love,\nSydney";
 
 function checkPassword() {
   let enteredPassword = document.getElementById('passwordInput').value;
@@ -8,6 +8,7 @@ function checkPassword() {
   
   if (enteredPassword === correctPassword) {
     messageElement.textContent = messageToShow;
+    <embed src="https://content.codecademy.com/courses/SemanticHTML/dog-on-beach.gif"></embed>
     messageElement.style.color = "green";
   } else {
     messageElement.textContent = "Incorrect password. Please try again.";
